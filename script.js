ec02b147e013bb859a99eb6cba68715170fe4a2d
Yuliana Velez <velezrodriguez2001@gmail.com>
2023-06-19 19:35:11 -0500

    Fixed Linters

--- script.js
+++ script.js
@@ -1,35 +1,34 @@
-  const hamburgerContainer = document.querySelector('#hamburgerContainer');
-  const nav =document.querySelector('nav')
-  const navigationLinks = document.querySelector('.navigationLinks');
-  const hamburgerBar = document.querySelectorAll(".hamburgerBar")
-  const closebutton = document.querySelector("#closebutton")
+const hamburgerContainer = document.querySelector('#hamburgerContainer');
+const nav = document.querySelector('nav');
+const navigationLinks = document.querySelector('.navigationLinks');
+const hamburgerBar = document.querySelectorAll('.hamburgerBar');
+const closebutton = document.querySelector('#closebutton');
 
-  hamburgerContainer.addEventListener('click', function() {
-    if (navigationLinks.classList.contains("onlyDesktop")){
-      navigationLinks.classList.remove("onlyDesktop")
-      nav.classList.add("navMobile")
-      for (let i=0; i< hamburgerBar.length; i++) {
-        console.log(hamburgerBar[i].classList)
-        hamburgerBar[i].classList.add("hidden")
-      }
-      closebutton.classList.remove("hidden")
-    }else {
-      nav.classList.remove("navMobile")
-      navigationLinks.classList.add("onlyDesktop")
-      for (let i=0; i< hamburgerBar.length; i++) {
-        hamburgerBar[i].classList.remove("hidden")
-      }
-      closebutton.classList.add("hidden")
+hamburgerContainer.addEventListener('click', () => {
+  if (navigationLinks.classList.contains('onlyDesktop')) {
+    navigationLinks.classList.remove('onlyDesktop');
+    nav.classList.add('navMobile');
+    for (let i = 0; i < hamburgerBar.length; i += 1) {
+      hamburgerBar[i].classList.add('hidden');
     }
-  });
-
-  function closewindow() {
-    nav.classList.remove("navMobile")
-      navigationLinks.classList.add("onlyDesktop")
-      for (let i=0; i< hamburgerBar.length; i++) {
-        hamburgerBar[i].classList.remove("hidden")
-      }
-    closebutton.classList.add("hidden");
+    closebutton.classList.remove('hidden');
+  } else {
+    nav.classList.remove('navMobile');
+    navigationLinks.classList.add('onlyDesktop');
+    for (let i = 0; i < hamburgerBar.length; i += 1) {
+      hamburgerBar[i].classList.remove('hidden');
+    }
+    closebutton.classList.add('hidden');
   }
+});
 
-  navigationLinks.addEventListener('click', closewindow);
+function closewindow() {
+  nav.classList.remove('navMobile');
+  navigationLinks.classList.add('onlyDesktop');
+  for (let i = 0; i < hamburgerBar.length; i += 1) {
+    hamburgerBar[i].classList.remove('hidden');
+  }
+  closebutton.classList.add('hidden');
+}
+
+navigationLinks.addEventListener('click', closewindow);
